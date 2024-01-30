6fcc7afb5ef426517760cd0111b228a517fa9f0f
Roman Fausel <111987884+RoMaN-CoLoGnE@users.noreply.github.com>
2024-01-30 16:34:24 +0100

    added projects

--- src/js/projectsSlider.js
+++ src/js/projectsSlider.js
@@ -1,13 +1,16 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  var mySwiper = new Swiper('.swiper', {
+  const mySwiper = new Swiper('.swiper', {
     effect: 'cards',
     grabCursor: true,
+    perSlideOffset: 20,
+    perSlideRotate: 2,
     initialSlide: 2,
     speed: 500,
-    loop: true,
+    // loop: true,
     rotate: true,
+    slideShadows: true,
     mousewheel: {
       invert: false,
     },
