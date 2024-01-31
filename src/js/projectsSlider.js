2ac3d777981fdf348aa5ea54131617580fc3fb52
Roman Fausel <111987884+RoMaN-CoLoGnE@users.noreply.github.com>
2024-01-31 11:32:45 +0100

    added projects

--- src/js/projectsSlider.js
+++ src/js/projectsSlider.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
     perSlideRotate: 2,
     initialSlide: 2,
     speed: 500,
-    // loop: true,
     rotate: true,
     slideShadows: true,
     mousewheel: {
